71c422655aac27fc4ae4626afd1e40f982c0434c
Michael Mwangi <michaelmichire2016@gmail.com>
2024-03-19 06:55:32 -0700

    Updates

--- models/state.py
+++ models/state.py
@@ -1,8 +1,38 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+
+"""A module that has the state class"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+import models
+import shlex
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from models.city import City
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """A class for State"""
+
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
+
+    @property
+    def cities(self):
+        variable = models.storage.all()
+        lista = []
+        res = []
+        for key in variable:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+
+            if (city[0] == 'City'):
+                lista.append(variable[key])
+
+        for elements in lista:
+            if (elements.state_id == self.id):
+                res.append(elements)
+
+        return (res)
