26be522f70ab9e0d482ad8e14034f57e07dee861
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-04-04 09:56:54 -0700

    Deploy static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,12 +8,12 @@ mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 chown -R ubuntu /data/
 chgrp -R ubuntu /data/
 
-sudo bash -c 'printf %s "server {
+printf %s "server {
     listen 80 default_server;
     listen [::]:80 default_server;
     add_header X-Served-By $hostname;
