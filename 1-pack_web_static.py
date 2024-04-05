caa33b1ff3efcada288316d8b58611016cfd68da
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-04-05 02:09:25 -0700

    Deploy static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,9 +9,15 @@ def do_pack():
 
     time = datetime.now()
     time_string = time.strftime('%Y%m%d%H%M%S')
-    
-    local('tar -cvzf versions/web_static_{}.tgz web_static'.format(time_string))
-    #local(f'tar -cvzf versions/web_static_{time_string}.tgz web_static')
 
-    file_path = f"versions/web_static_{time_string}.tgz"
-    file_size = os.path.getsize(file_path)
+    output_path = 'versions/web_static_{}.tgz'.format(time_string)
+
+    try:
+        print("Packing web_static to {}".format(output_path))
+        local('tar -cvzf {} web_static'.format(output_path))
+        file_size = os.path.getsize(output_path)
+        print("web_static packed: {} -> {} Bytes".format(output_path, file_size))
+        return output_path
+    except Exception as e:
+        print("Error occurred during packing: {}".format(str(e)))
+        return None
