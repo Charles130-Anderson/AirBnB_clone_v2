e3e379e7abf98c3d999358bfdccb9644e9dfc976
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-04-20 12:41:24 -0700

    web-flask

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -48,7 +48,7 @@ def number_route(n):
     """
     Route to display “n is a number” only if n is an integer.
     """
-    return "{} is a number".format(n)
+    return "{:d} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
