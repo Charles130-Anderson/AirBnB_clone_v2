0b6e6545af97d89c63169e75a37e8ce1355e6fef
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-04-21 04:32:33 -0700

    web-flask

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -24,9 +24,9 @@ def display_c(text):
     return 'C {}'.format(text.replace('_', ' '))
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def display_python(text):
+def display_python(text='is cool'):
     """Display 'Python' followed by the value of the text variable."""
     return 'Python {}'.format(text.replace('_', ' '))
 
